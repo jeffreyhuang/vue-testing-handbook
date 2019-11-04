76fa7ddfcb2620a5872f26088ee4abc1a25c624e
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-11-04 13:42:40 +0800

    update component and tests

--- tests/unit/Greeting.spec.js
+++ tests/unit/Greeting.spec.js
@@ -1,26 +1,10 @@
-import Vue from 'vue'
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Greeting from '@/components/Greeting'
 
-const Child = Vue.component('Child', {
-  name: 'Child',
-  template: '<div>Child component</div>'
-})
-
-const Parent = Vue.component('Parent', {
-  name: 'Parent',
-  template: '<div><Child /></div>'
-})
-
-describe('Greeting.vue', () => {
+describe('Greeting', () => {
   it('renders a greeting', () => {
-    // const mountWrapper = mount(Parent)
-    // console.log(mountWrapper.html())
+    const wrapper = mount(Greeting)
 
-    const shallowWrapper = shallowMount(Parent)
-    console.log(shallowWrapper.html())
-
-    // console.log(wrapper.html())
-    // expect(wrapper.text()).toMatch('Vue without TDD')
+    expect(wrapper.text()).toMatch('Vue and TDD')
   })
 })
